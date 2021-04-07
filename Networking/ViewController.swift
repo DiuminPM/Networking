393003c0c75418ef98f109ddcad697aac72a7650
DiuminPM <75372207+DiuminPM@users.noreply.github.com>
2021-04-07 17:05:35 +0300

    add buttons "get" and "post" and make get request

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -6,27 +6,26 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
-    var image = UIImageView()
     let tapButton = UIButton()
-    let activityIndicator = UIActivityIndicatorView()
+    let tapGetButton = UIButton()
+    let tapPostButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         initialized()
+        setupNavigationBar()
         
         // Do any additional setup after loading the view.
     }
     private func initialized() {
         view.backgroundColor = .gray
-        image.frame = view.bounds
-        image.isHidden = true
-        view.addSubview(image)
-        tapButton.isHidden = false
         tapButton.addTarget(self, action: #selector(fetchedImage), for: .touchUpInside)
-        tapButton.frame = CGRect(x: 16, y: view.frame.height/2 - 150, width: view.frame.width - 32, height: 300)
+        view.addSubview(tapButton)
+        tapButton.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
         tapButton.layer.borderWidth = 1
         tapButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         tapButton.layer.cornerRadius = 10
@@ -35,30 +34,74 @@ class ViewController: UIViewController {
         tapButton.titleLabel?.textAlignment = .center
         view.addSubview(tapButton)
         tapButton.setTitle("Tap to download an Image", for: .normal)
-        activityIndicator.isHidden = true
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.frame = CGRect(x: view.frame.width/2 - 25, y: view.frame.height/2 - 25, width: 50, height: 50)
-        activityIndicator.color = .blue
-        activityIndicator.transform = CGAffineTransform(scaleX: 3, y: 3)
-        view.addSubview(activityIndicator)
+        tapButton.snp.makeConstraints { (maker) in
+            maker.top.equalTo(view).inset(150)
+            maker.right.left.equalTo(view).inset(16)
+            maker.height.equalTo(150)
+        }
+        tapGetButton.addTarget(self, action: #selector(getRequest), for: .touchUpInside)
+        view.addSubview(tapGetButton)
+        tapGetButton.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        tapGetButton.layer.borderWidth = 1
+        tapGetButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        tapGetButton.layer.cornerRadius = 10
+        tapGetButton.titleLabel?.font = .systemFont(ofSize: 46)
+        tapGetButton.titleLabel?.numberOfLines = 0
+        tapGetButton.titleLabel?.textAlignment = .center
+        view.addSubview(tapGetButton)
+        tapGetButton.setTitle("GET", for: .normal)
+        tapGetButton.snp.makeConstraints { (maker) in
+            maker.top.equalTo(tapButton.snp.bottom).inset(-16)
+            maker.right.left.equalTo(view).inset(16)
+            maker.height.equalTo(150)
+        }
+        tapPostButton.addTarget(self, action: #selector(postRequest), for: .touchUpInside)
+        view.addSubview(tapPostButton)
+        tapPostButton.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        tapPostButton.layer.borderWidth = 1
+        tapPostButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        tapPostButton.layer.cornerRadius = 10
+        tapPostButton.titleLabel?.font = .systemFont(ofSize: 46)
+        tapPostButton.titleLabel?.numberOfLines = 0
+        tapPostButton.titleLabel?.textAlignment = .center
+        view.addSubview(tapPostButton)
+        tapPostButton.setTitle("POST", for: .normal)
+        tapPostButton.snp.makeConstraints { (maker) in
+            maker.top.equalTo(tapGetButton.snp.bottom).inset(-16)
+            maker.right.left.equalTo(view).inset(16)
+            maker.height.equalTo(150)
+        }
     }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = "Controller"
+    }
+    
     @objc func fetchedImage() {
-        image.isHidden = false
-        tapButton.isHidden = true
-        activityIndicator.isHidden = false
-        activityIndicator.startAnimating()
-        
-        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
+        navigationController?.pushViewController(ImageViewController(), animated: true)
+    }
+    
+    @objc func getRequest() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         let session = URLSession.shared
         session.dataTask(with: url) { (data, response, error) in
-            if let data = data, let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.activityIndicator.stopAnimating()
-                    self.image.image = image
-                }
+            guard let response = response,
+            let data = data else { return }
+            print(response)
+            print(data)
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
             }
-        }.resume()
+            catch {
+                print(error)
+            }
+        } .resume()
     }
+    @objc func postRequest() {
+        
+    }
+    
 
 
 }
